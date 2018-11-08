b160a87233f88a5954af4c83dbd4e77e74c4cae3
catkinmu <992746366@qq.com>
2018-11-08 20:26:11 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  background-color: #f1f1f1;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Layout class="layout">
+    <Header class="layout-hidder">Header</Header>
+    <Layout>
+      <Sider hide-trigger>Sider</Sider>
+      <Content class="layout-content">Content</Content>
+    </Layout>
+  </Layout>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style scoped>
+.layout {
+  height: 100%;
+  /* border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden; */
+}
+.layout-hidder,
+.layout-sider {
+  width: 100%;
+}
+.layout-content {
+  margin: 10px;
+  padding: 10px;
+  background: #ffffff;
+  /* background-clip: content-box; */
+}
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+}
+.layout-nav {
+  width: 420px;
+  margin: 0 auto;
+  margin-right: 20px;
+}
+</style>
